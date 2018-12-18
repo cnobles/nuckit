5240f4a85e869d19800a36a303b7128512129dff
Christopher Nobles <cnobles@pennmedicine.upenn.edu>
2018-12-18 16:17:32 -0500

    Changes to control mod.

--- nuckit_ctrl_lib/setup.py
+++ nuckit_ctrl_lib/setup.py
@@ -4,5 +4,7 @@ setup(name = 'nuckit_ctrl_lib',
       packages = [ 'nuckit_ctrl_lib', 'nuckit_ctrl_lib.scripts' ],
       entry_points = { 'console_scripts' : [
           'nuc = nuckit_ctrl_lib.scripts.command:main'
-          ] }
+          ] },
+      use_scm_version = True,
+      setup_requires = ['setuptools_scm']
      )
