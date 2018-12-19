5a73756447ad94b858bb65a801234ef9d5b4bfeb
Christopher Nobles <cnobles@pennmedicine.upenn.edu>
2018-12-19 12:36:52 -0500

    Changes to library for testing.

--- nuckit_ctrl_lib/nuckit_ctrl_lib/__init__.py
+++ nuckit_ctrl_lib/nuckit_ctrl_lib/__init__.py
@@ -7,8 +7,8 @@ import sys
 import csv
 
 from pathlib import Path
-from pkg_resources import get_distribution
+#from pkg_resources import get_distribution
 
-from semantic_version import Version
+#from semantic_version import Version
 
-__version__ = str(Version.coerce(get_distribution('nuckit').version))
+#__version__ = str(Version.coerce(get_distribution('nuckit').version))
--- nuckit_ctrl_lib/setup.py
+++ nuckit_ctrl_lib/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name = 'nuckit_ctrl_lib',
-      packages = [ 'nuckit_ctrl_lib', 'nuckit_ctrl_lib.scripts' ],
-      entry_points = { 'console_scripts' : [
-          'nuc = nuckit_ctrl_lib.scripts.command:main'
-          ] },
-      use_scm_version = True,
-      setup_requires = ['setuptools_scm']
-     )
+setup(
+    name = 'nuckit_ctrl_lib',
+    packages = [ 'nuckit_ctrl_lib', 'nuckit_ctrl_lib.scripts'  ],
+    entry_points = { 'console_scripts' : [
+        'nuc = nuckit_ctrl_lib.scripts.command:main'
+        ] },
+#    use_scm_version = True,
+#    setup_requires = ['setuptools_scm']
+)
